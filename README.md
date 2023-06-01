86663fe567726ca7dfcbd10c173adbc4da6adb2f
Vipertech <74778466+vipertechofficial@users.noreply.github.com>
2023-06-01 14:51:05 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,222 @@
-# SetFixed
-A High-Performance Approach to Managing Pixel Art Selection State
+# Boolean Array (6.2kB minified & 0 dep.)
+
+## Install
+
+```script
+npm install @asaitama/boolean-array
+```
+
+## Import
+
+### Browser
+```javascript
+// First you'll need to import the script;
+var BitArray = window.BitArray;
+var SetFixed = window.SetFixed;
+```
+
+### NodeJS
+```javascript
+import { BitArray, SetFixed } from "@asaitama/boolean-array";
+```
+
+## BitArray
+BitArray is a useful data structure that allows you to manipulate an array of bits efficiently. This object is perfect for situations where you need to work with bits directly. For instance, it can be useful in coding problems related to bit manipulation or when dealing with binary data. It provides methods to read, write, clear and set individual bits, as well as other utility functions.
+
+## SetFixed
+SetFixed is a set data structure implemented using the BitArray object. This is an efficient and compact data structure for storing non-repeated elements. SetFixed provides a variety of methods to manipulate the set such as adding, deleting, checking for membership, and iterating over the set.
+
+## BitArray Documentation
+The BitArray object provides an efficient bit-level manipulation of a typed array. It offers methods for setting, getting and manipulating individual bits within the array.
+
+### Constructor
+#### `BitArray(s)`
+The constructor function takes a parameter `s`, which represents the size of the BitArray. This is used to initialize the internal Uint32Array. The function also initializes the `M_OR_` and `M_AND_` Uint32Arrays, which are used as masks for setting and clearing bits.
+
+```javascript
+let bitArray = new BitArray(64);
+```
+
+### Properties
+
+#### `readFourBytes`
+This getter function returns a function that reads a block of four bytes starting at index `i`, returning an array of the indices of the bits that are set within this block.
+
+javascript
+let bitArray = new BitArray(64);
+let read = bitArray.readFourBytes;
+let result = read(4);
+
+#### `readBit`
+This getter function returns a function that reads the bit at index `i`, returning a boolean indicating whether the bit is set.
+
+```javascript
+let bitArray = new BitArray(64);
+let read = bitArray.readBit;
+let result = read(4);
+```
+
+#### `writeBit1`
+This getter function returns a function that sets the bit at index `i` to 1.
+
+```javascript
+let bitArray = new BitArray(64);
+let write = bitArray.writeBit1;
+write(4);
+```
+
+#### `writeBit0`
+This getter function returns a function that sets the bit at index `i` to 0.
+
+```javascript
+let bitArray = new BitArray(64);
+let write = bitArray.writeBit0;
+write(4);
+```
+
+#### `clear`
+This getter function returns a function that clears all bits in the BitArray.
+
+```javascript
+let bitArray = new BitArray(64);
+let clear = bitArray.clear;
+clear();
+```
+
+#### `charge`
+This getter function returns a function that sets all bits in the BitArray to 1.
+
+```javascript
+let bitArray = new BitArray(64);
+let charge = bitArray.charge;
+charge();
+```
+
+#### `length`
+This getter function returns the length of the BitArray.
+
+```javascript
+let bitArray = new BitArray(64);
+let length = bitArray.length;
+console.log(length);
+```
+
+Please note that all methods described above are read-only and non-configurable. This means they cannot be re-assigned or changed.
+
+## SetFixed Documentation
+The SetFixed object provides a set data structure that allows efficient addition, deletion, and checks for membership. It offers methods for setting, getting and manipulating individual elements within the set.
+
+### Constructor
+
+#### `SetFixed(size)`
+The constructor function takes a parameter size, which represents the size of the SetFixed. This is used to initialize the internal BitArray. **If size is an object (presumably an array-like object), then the SetFixed is initialized with those elements**.
+
+```javascript
+let setFixed = new SetFixed(64);
+```
+
+### Properties
+#### `size`
+This getter function returns the size of the SetFixed set, which is the number of elements in the set.
+
+```javascript
+let setFixed = new SetFixed(64);
+let size = setFixed.size;
+console.log(size);
+```
+
+#### `length`
+This getter function returns the length of the BitArray used internally.
+
+```javascript
+let setFixed = new SetFixed(64);
+let length = setFixed.length;
+console.log(length);
+```
+
+#### `indexes`
+This getter function returns an array of the indices of the bits that are set within the BitArray.
+
+```javascript
+let setFixed = new SetFixed(64);
+let indexes = setFixed.indexes;
+console.log(indexes);
+```
+
+#### `has`
+This getter function returns a function that checks if a given index i is set within the BitArray.
+
+```javascript
+let setFixed = new SetFixed(64);
+let has = setFixed.has;
+console.log(has(4));
+```
+
+#### `add`
+This getter function returns a function that adds a given index i to the SetFixed.
+If the index given is beyond the capacity of the instance, it will be resized correctly with a margin.
+Don't worry about resizing or memory consumption, it is far more efficient than a traditional new Set() instance object if you deal with indexes below 1/4 million (indexes from 0 to 250000 will represent only a few 31kB in memory)
+
+> It is as much as **2-3x faster and 4-6x lighter** for the JS Engine when used for managing pixel art selection state on canvas of 512x512 pixels than "new Set()" but is **limited to entire integer within a range**!!!
+
+```javascript
+let setFixed = new SetFixed(64);
+let add = setFixed.add;
+add(4);
+```
+
+#### `delete`
+This getter function returns a function that deletes a given index i from the SetFixed.
+
+```javascript
+let setFixed = new SetFixed(64);
+let del = setFixed.delete;
+del(4);
+```
+
+#### `clear`
+This getter function returns a function that clears all elements in the SetFixed.
+
+```javascript
+let setFixed = new SetFixed(64);
+let clear = setFixed.clear;
+clear();
+```
+
+#### `charge`
+This getter function returns a function that sets all bits in the BitArray to 1.
+
+```javascript
+let setFixed = new SetFixed(64);
+let charge = setFixed.charge;
+charge();
+```
+
+#### `forEach`
+This getter function returns a function that iterates over the elements in the SetFixed, applying a given function.
+
+```javascript
+let setFixed = new SetFixed(64);
+let forEach = setFixed.forEach;
+forEach(console.log);
+```
+
+#### `map`
+This getter function returns a function that maps a given function over the elements in the SetFixed.
+
+```javascript
+let setFixed = new SetFixed(64);
+let map = setFixed.map;
+console.log(map(x => x * 2));
+```
+
+#### `filter`
+This getter function returns a function that filters the elements in the SetFixed based on a given function.
+
+```javascript
+let setFixed = new SetFixed(64);
+let filter = setFixed.filter;
+console.log(filter(x => x % 2 === 0));
+```
+
+Please note that all methods described above are read-only and non-configurable. This means they cannot be re-assigned or changed.
---
+++ index.js
@@ -0,0 +1,373 @@
+/*
+MIT License
+Copyright (c) 2023 Affolter Matias
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+function BitArray(s) {
+    s = (s | 0) >>> 0;
+    this.l_ = s|0;
+    this.a_ = new Uint32Array((this.l_ + 31 | 0) >>> 5);
+    this.M_OR_ = Uint32Array.of(
+        0b00000000000000000000000000000001,
+        0b00000000000000000000000000000010,
+        0b00000000000000000000000000000100,
+        0b00000000000000000000000000001000,
+        0b00000000000000000000000000010000,
+        0b00000000000000000000000000100000,
+        0b00000000000000000000000001000000,
+        0b00000000000000000000000010000000,
+        0b00000000000000000000000100000000,
+        0b00000000000000000000001000000000,
+        0b00000000000000000000010000000000,
+        0b00000000000000000000100000000000,
+        0b00000000000000000001000000000000,
+        0b00000000000000000010000000000000,
+        0b00000000000000000100000000000000,
+        0b00000000000000001000000000000000,
+        0b00000000000000010000000000000000,
+        0b00000000000000100000000000000000,
+        0b00000000000001000000000000000000,
+        0b00000000000010000000000000000000,
+        0b00000000000100000000000000000000,
+        0b00000000001000000000000000000000,
+        0b00000000010000000000000000000000,
+        0b00000000100000000000000000000000,
+        0b00000001000000000000000000000000,
+        0b00000010000000000000000000000000,
+        0b00000100000000000000000000000000,
+        0b00001000000000000000000000000000,
+        0b00010000000000000000000000000000,
+        0b00100000000000000000000000000000,
+        0b01000000000000000000000000000000,
+        0b10000000000000000000000000000000
+    );
+
+    this.M_AND_ = Uint32Array.of(
+        0b11111111111111111111111111111110,
+        0b11111111111111111111111111111101,
+        0b11111111111111111111111111111011,
+        0b11111111111111111111111111110111,
+        0b11111111111111111111111111101111,
+        0b11111111111111111111111111011111,
+        0b11111111111111111111111110111111,
+        0b11111111111111111111111101111111,
+        0b11111111111111111111111011111111,
+        0b11111111111111111111110111111111,
+        0b11111111111111111111101111111111,
+        0b11111111111111111111011111111111,
+        0b11111111111111111110111111111111,
+        0b11111111111111111101111111111111,
+        0b11111111111111111011111111111111,
+        0b11111111111111110111111111111111,
+        0b11111111111111101111111111111111,
+        0b11111111111111011111111111111111,
+        0b11111111111110111111111111111111,
+        0b11111111111101111111111111111111,
+        0b11111111111011111111111111111111,
+        0b11111111110111111111111111111111,
+        0b11111111101111111111111111111111,
+        0b11111111011111111111111111111111,
+        0b11111110111111111111111111111111,
+        0b11111101111111111111111111111111,
+        0b11111011111111111111111111111111,
+        0b11110111111111111111111111111111,
+        0b11101111111111111111111111111111,
+        0b11011111111111111111111111111111,
+        0b10111111111111111111111111111111,
+        0b01111111111111111111111111111111
+    );
+}
+
+Object.defineProperty(BitArray.prototype, 'readFourBytes', {
+    get: function() {
+        "use strict";
+        return function(i) {
+            "use strict";
+            i = (i | 0) >>> 0;
+            var a = [], b = this.a_[(i | 0) >>> 5];
+
+            if((b & this.M_OR_[0]|0) == (this.M_OR_[0]|0)){ a.push(i|0) }
+            if((b & this.M_OR_[1]|0) == (this.M_OR_[1]|0)){ a.push(i+1|0) }
+            if((b & this.M_OR_[2]|0) == (this.M_OR_[2]|0)){ a.push(i+2|0) }
+            if((b & this.M_OR_[3]|0) == (this.M_OR_[3]|0)){ a.push(i+3|0) }
+            if((b & this.M_OR_[4]|0) == (this.M_OR_[4]|0)){ a.push(i+4|0) }
+            if((b & this.M_OR_[5]|0) == (this.M_OR_[5]|0)){ a.push(i+5|0) }
+            if((b & this.M_OR_[6]|0) == (this.M_OR_[6]|0)){ a.push(i+6|0) }
+            if((b & this.M_OR_[7]|0) == (this.M_OR_[7]|0)){ a.push(i+7|0) }
+            if((b & this.M_OR_[8]|0) == (this.M_OR_[8]|0)){ a.push(i+8|0) }
+            if((b & this.M_OR_[9]|0) == (this.M_OR_[9]|0)){ a.push(i+9|0) }
+            if((b & this.M_OR_[10]|0) == (this.M_OR_[10]|0)){ a.push(i+10|0) }
+            if((b & this.M_OR_[11]|0) == (this.M_OR_[11]|0)){ a.push(i+11|0) }
+            if((b & this.M_OR_[12]|0) == (this.M_OR_[12]|0)){ a.push(i+12|0) }
+            if((b & this.M_OR_[13]|0) == (this.M_OR_[13]|0)){ a.push(i+13|0) }
+            if((b & this.M_OR_[14]|0) == (this.M_OR_[14]|0)){ a.push(i+14|0) }
+            if((b & this.M_OR_[15]|0) == (this.M_OR_[15]|0)){ a.push(i+15|0) }
+            if((b & this.M_OR_[16]|0) == (this.M_OR_[16]|0)){ a.push(i+16|0) }
+            if((b & this.M_OR_[17]|0) == (this.M_OR_[17]|0)){ a.push(i+17|0) }
+            if((b & this.M_OR_[18]|0) == (this.M_OR_[18]|0)){ a.push(i+18|0) }
+            if((b & this.M_OR_[19]|0) == (this.M_OR_[19]|0)){ a.push(i+19|0) }
+            if((b & this.M_OR_[20]|0) == (this.M_OR_[20]|0)){ a.push(i+20|0) }
+            if((b & this.M_OR_[21]|0) == (this.M_OR_[21]|0)){ a.push(i+21|0) }
+            if((b & this.M_OR_[22]|0) == (this.M_OR_[22]|0)){ a.push(i+22|0) }
+            if((b & this.M_OR_[23]|0) == (this.M_OR_[23]|0)){ a.push(i+23|0) }
+            if((b & this.M_OR_[24]|0) == (this.M_OR_[24]|0)){ a.push(i+24|0) }
+            if((b & this.M_OR_[25]|0) == (this.M_OR_[25]|0)){ a.push(i+25|0) }
+            if((b & this.M_OR_[26]|0) == (this.M_OR_[26]|0)){ a.push(i+26|0) }
+            if((b & this.M_OR_[27]|0) == (this.M_OR_[27]|0)){ a.push(i+27|0) }
+            if((b & this.M_OR_[28]|0) == (this.M_OR_[28]|0)){ a.push(i+28|0) }
+            if((b & this.M_OR_[29]|0) == (this.M_OR_[29]|0)){ a.push(i+29|0) }
+            if((b & this.M_OR_[30]|0) == (this.M_OR_[30]|0)){ a.push(i+30|0) }
+            if((b & this.M_OR_[31]|0) == (this.M_OR_[31]|0)){ a.push(i+31|0) }
+
+
+            return a;
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(BitArray.prototype, 'readBit', {
+    get: function() {
+        "use strict";
+        return function(i) {
+            "use strict";
+            i = (i | 0) >>> 0;
+            var m_or = this.M_OR_[i & 31];
+            i = (i | 0) >>> 5;
+            return (this.a_[i|0] & m_or | 0) == (m_or|0);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(BitArray.prototype, 'writeBit1', {
+    get: function() {
+        "use strict";
+        return function(i) {
+            "use strict";
+            i = (i | 0) >>> 0;
+            var m_or = this.M_OR_[i & 31];
+            i = (i | 0) >>> 5;
+            this.a_[i|0] = this.a_[i|0] | m_or;
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(BitArray.prototype, 'writeBit0', {
+    get: function() {
+        "use strict";
+        return function(i) {
+            "use strict";
+            i = (i | 0) >>> 0;
+            var m_and = this.M_AND_[i & 31];
+            i = (i | 0) >>> 5;
+            this.a_[i|0] = this.a_[i|0] & m_and;
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+
+Object.defineProperty(BitArray.prototype, 'clear', {
+    get: function() {
+        "use strict";
+        return function() {
+            "use strict";
+            this.a_.fill(0);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(BitArray.prototype, 'charge', {
+    get: function() {
+        "use strict";
+        return function() {
+            "use strict";
+            this.a_.fill(0xFFFFFFFF);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(BitArray.prototype, 'length', {
+    get: function() {
+        "use strict";
+        return (this.l_ | 0) >>> 0;
+    },
+    enumerable: false,
+    configurable: false
+});
+
+var SetFixed = function(size){
+    "use strict";
+    if (!(this instanceof SetFixed)) {
+        return new SetFixed(size);
+    }
+    if(typeof size == "object") {
+        this.set_ = new Set(Array.from(size));
+        this.s_ = this.set_.size;
+        this.indexes_ = Array.from(this.set_);
+        this.max_ = 0;
+        for(var i = 0, l = this.indexes_.length|0; (i|0) < (l|0); i = i + 1 | 0){if((this.max_|0) < (this.indexes_[i|0]|0)){ this.max_ = this.indexes_[i|0]|0; }}
+        this.a_ = new BitArray(this.max_);
+        for(var i = 0, l = this.indexes_.length|0; (i|0) < (l|0); i = i + 1 | 0){this.a_.writeBit1(this.indexes_[i|0]|0); }
+
+        delete this.set_;
+        delete this.indexes_;
+        delete this.max_;
+    }else {
+
+        this.a_ = new BitArray(size);
+        this.s_ = 0;
+    }
+};
+
+Object.defineProperty(SetFixed.prototype, 'size', {
+    get: function() {
+        "use strict";
+        return (this.s_ | 0) >>> 0;
+    },
+    enumerable: false,
+    configurable: false
+});
+Object.defineProperty(SetFixed.prototype, 'length', {
+    get: function() {
+        "use strict";
+        return (this.a_.length | 0) >>> 0;
+    },
+    enumerable: false,
+    configurable: false
+});
+Object.defineProperty(SetFixed.prototype, 'indexes', {
+    get: function() {
+        "use strict";
+        var a = [], l = this.length|0, i = 0;
+        for (; (i|0) < (l|0); i = (i + 32 | 0)>>>0) {
+            a.push.apply(a, this.a_.readFourBytes(i|0));
+        }
+        return a;
+    },
+    enumerable: false,
+    configurable: false
+});
+Object.defineProperty(SetFixed.prototype, 'has', {
+    get: function() {  "use strict"; return function(i) {
+        "use strict";
+        i = (i|0) >>> 0;
+        return this.a_.readBit(i|0);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(SetFixed.prototype, 'add', {
+    get: function() {  "use strict"; return function(i) {
+        "use strict";
+
+        i = (i|0) >>> 0;
+        if((this.length|0) < (i|0)){
+            var a = this.indexes, l = a.length|0, x = 0;
+            this.a_ = new BitArray(i+i|0);
+            for(;(x|0)<(l|0); x = x + 1 | 0) {
+                this.a_.writeBit1(a[x|0]|0);
+            }
+            this.s_ = (a.length|0)>>>0;
+        }
+
+        if(!this.a_.readBit(i | 0)){
+            this.s_ = (this.s_ + 1 | 0) >>> 0;
+        }
+
+        this.a_.writeBit1(i | 0);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(SetFixed.prototype, 'delete', {
+    get: function() {  "use strict"; return function(i) {
+        "use strict";
+        i = (i|0) >>> 0;
+        if(this.a_.readBit(i | 0)){
+            this.a_.writeBit0(i | 0);
+            this.s_ = (this.s_ - 1 | 0) >>> 0;
+        }
+
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(SetFixed.prototype, 'clear', {
+    get: function() {  "use strict"; return function() {
+        "use strict";
+        this.a_.clear();
+        this.s_ = 0;
+    }}
+});
+
+Object.defineProperty(SetFixed.prototype, 'charge', {
+    get: function() {  "use strict"; return function() {
+        "use strict";
+        this.a_.charge();
+        this.s_ = this.a_.length;
+    }}
+});
+
+Object.defineProperty(SetFixed.prototype, 'forEach', {
+    get: function() {
+        "use strict"; return function(func) {
+            "use strict";
+            this.indexes.forEach(func);
+        }
+    },
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(SetFixed.prototype, 'map', {
+    get: function() {
+        "use strict";
+        return function(func) {
+            "use strict";
+            return this.indexes.map(func);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+Object.defineProperty(SetFixed.prototype, 'filter', {
+    get: function() {
+        "use strict";
+        return function(func) {
+            "use strict";
+            return this.indexes.filter(func);
+    }},
+    enumerable: false,
+    configurable: false
+});
+
+if(module.exports) {
+
+    module.exports = {SetFixed: SetFixed, BitArray: BitArray};
+}else {
+    window.BitArray = BitArray;
+    window.SetFixed = SetFixed;
+}
